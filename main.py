3827bf17cd7823e2d8f31408c830ff52b428d1ac
DominiqueDavid <109325294+DominiqueDavid@users.noreply.github.com>
2024-08-22 19:15:02 -0400

    Intialization

---
+++ main.py
@@ -0,0 +1,18 @@
+#pip install Flask
+#install flask-socketio
+
+from flask import Flask, render_template,request, session, redirect
+from flask_socketio import join_room,leave_room,send, SocketIO
+import random
+from string import ascii_uppercase
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "key"
+socketio = SocketIO(app)
+
+if __name__ == "__main__":
+    socketio.run(app,debug=True)
+
+
+
